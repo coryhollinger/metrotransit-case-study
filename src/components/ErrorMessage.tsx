2e46066bc98893906d485a7f80534aa11a988734
Cory Hollinger <cory.hollinger@target.com>
2025-02-23 23:35:20 -0600

    fix error boundary

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,18 +1,19 @@
 import { Box, Typography } from "@mui/material";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { ERROR_BOUNDARY_MESSAGE } from "../strings";
 
-const ErrorMessage = ({ error }: { error: string }) => {
-  return error ? (
+const ErrorMessage = ({ error }: { error: Error }) => {
+  return (
     <Box
       sx={{ textAlign: "center", padding: "20px" }}
       data-testid="error-message"
     >
       <Typography variant="h4">
         <ErrorOutlineIcon fontSize="large" sx={{ mr: 1 }} />
-        {error}
+        {error ? error.message : ERROR_BOUNDARY_MESSAGE}
       </Typography>
     </Box>
-  ) : null;
+  );
 };
 
 export default ErrorMessage;
